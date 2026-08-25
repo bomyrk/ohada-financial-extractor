6f97239bef3a74f7be23354bffa0aef12ff34a23
Bomyr Kamguia <bomyrk@gmail.com>
2026-08-25 18:52:07 -0400

    fix: rigth version naming

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="ohada-financial-extractor",
-    version="0.1.6",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-)
