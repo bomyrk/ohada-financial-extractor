de1416ffc84b322084748099d07d013f3b6b996f
Bomyr Kamguia <bomyrk@gmail.com>
2026-08-25 08:23:24 -0400

    fix: use static version in __init__.py

--- src/ohada_extractor/__init__.py
+++ src/ohada_extractor/__init__.py
@@ -20,10 +20,7 @@ Example:
     >>> json_output = data.to_json()
 """
 
-try:
-    from ._version import version as __version__
-except ImportError:
-    __version__ = "unknown"
+__version__ = "0.1.5"
 
 __author__ = "Kamguia Wabo Leonel B. "
 
